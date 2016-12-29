baa4200460821e2dac68729ca986a3c8e5b23182
AnthonyHaffey <anthony.haffey@gmail.com>
2016-12-29 17:11:46 +0000

    Changed directories to work in the new_location

--- plotSaliencyData (1).R
+++ plotSaliencyData (1).R
@@ -3,7 +3,8 @@ library(R.matlab)
 library(ggplot2)
 
 # load in data from matlab
-d <- readMat('C:/Users/Anthony/Dropbox/Anthony/Bhismalab/Papers/FV_2010_Data/ratiosPsy_and_Image.mat')
+setwd("C:/Users/Anthony/Dropbox/Anthony/wamp/www/EyeTracking_PlosOne_2017") # you need to change this to whichever directory you're storing your file
+d <- readMat('ratiosPsy_and_Image.mat')
 
 # transpose image vaiables
 d$ratioRMS <- t(d$ratioRMS)
@@ -16,4 +17,4 @@ val <- d$ratioValence[,2]
 data <- data.frame(ratio = (c(val,d$ratioArousal,d$ratioRMS,d$ratioLocalRMS,d$ratioSalience)), measure = (rep(c("rating - valence","rating - arousal","RMS - global","RMS - local","Salience - Koch"),each = 40)))
 bp <- ggplot(data, aes(x=measure,y=ratio))+geom_boxplot()+ylab("social / non-social ratio")
 #ggsave(bp, filename = "C:/Users/Anthony/Dropbox/Anthony/Bhismalab/Papers/FV_2010_Data/socnonsoc_ratio_measures.pdf", width=10, height=10)
-ggsave(bp, filename = "C:/Users/Anthony/Dropbox/Anthony/Bhismalab/Papers/FV_2010_Data/Figure_1.png", width=10, height=10)
+ggsave(bp, filename = "C:/Users/Anthony/Dropbox/Anthony/wamp/www/EyeTracking_PlosOne_2017/Figure_1.png", width=10, height=10)
